6b251b20a5bd2ef9971c961eda637fe3324721e4
Loïc PILLER <loicpiller@mail.fr>
2026-03-30 22:19:25 +0200

    feat(routing): register new pages and actions

--- config/routes.php
+++ config/routes.php
@@ -6,4 +6,15 @@ return [
     '/inscription' => 'UserController@register',
     '/deconnexion' => 'UserController@logout',
     '/mon-compte' => 'UserController@profile',
+    '/livres-echangeables' => 'BookController@index',
+    '/livre/{id}' => 'BookController@bookDetails',
+    '/livre/{id}/edition' => 'BookController@edit',
+    '/livre/{id}/supprimer' => 'BookController@delete',
+    'profile/{id}' => 'UserController@publicProfile',
+    'messages/{contactId}' => 'MessagingController@index',
+    'messages' => 'MessagingController@index',
+
+    'send-message/{receiverId}' => 'MessagingController@send',
+    'update-picture' => 'UserController@updatePicture',
+    'update-profile' => 'UserController@updateProfile',
 ];
